57363456de8393568310e082ad48a35b3c34a3ee
David Österberg <davost@gmail.com>
2012-06-08 18:27:41 +0200

    Add docstring to shadow.py

--- shadow.py
+++ shadow.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Simple GUI for shadow flicker calculations.
+Use this code at your own risk!
+
+## Usage:
+Just run
+>python2 shadow_gui.py
+
+
+## Website:
+https://github.com/davidosterberg/shadowtime
+
+## Licence:
+(C) 2012 David Österberg
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 from pylab import *
 from matplotlib.nxutils import points_inside_poly
