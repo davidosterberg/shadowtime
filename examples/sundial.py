8d37cbe84a955b88db895a6bee1bcb17954ebb2a
David Österberg <davost@gmail.com>
2012-06-11 23:17:47 +0200

    Update sundial example

--- examples/sundial.py
+++ examples/sundial.py
@@ -3,18 +3,15 @@
 import shadowtime
 from pylab import *
 
-phi = -127./180*pi
-delta = 36.4/180*pi
+phi = 14.150704/180*pi
+delta = 57.749689/180*pi
 h = 1.
 
+subplot(111, autoscale_on=False,aspect='equal')
 for hour in range(24): 
     x,y = shadowtime.analemma(phi,delta,h,hour)
     x = x/h
     y = y/h
-    x[x < -4] = None
-    y[x < -4] = None
-    x[x > 4] = None
-    y[x > 4] = None    
     plot(x,y,'k')
 
 plot(0,0,'ko')    
@@ -23,19 +20,18 @@ x,y = shadowtime.day_path(phi,delta,h,365+shadowtime.solstice_offset)
 x = x/h
 y = y/h
 
-x[x < -4] = None
-y[x < -4] = None
-x[x > 4] = None
-y[x > 4] = None 
 plot(x,y,'k',linewidth=2.0)
 x,y = shadowtime.day_path(phi,delta,h,365/2+shadowtime.solstice_offset)
 x = x/h
 y = y/h
 
-x[x < -4] = None
-y[x < -4] = None
-x[x > 4] = None
-y[x > 4] = None 
 plot(x,y,'k',linewidth=2.0)
+plot([-0.5,0.5],[-1,-1],'k')
+plot([-0.5,0.5],[-2,-2],'k')
+plot([-0.5,-0.5],[-2,-1],'k')
+plot([0.5,0.5],[-2,-1],'k')
 
+xlim((-12,12))
+ylim((-12,18))
+axis('off')
 show()
